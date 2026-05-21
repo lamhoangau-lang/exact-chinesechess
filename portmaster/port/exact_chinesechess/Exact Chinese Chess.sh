1f2616a418ca0299026acfc56fd7695aafa932db
J <jckhng@gmail.com>
2026-05-21 23:36:44 +0800

    gptk passthrough and launcher script fixes

--- portmaster/port/exact_chinesechess/Exact Chinese Chess.sh
+++ portmaster/port/exact_chinesechess/Exact Chinese Chess.sh
@@ -18,26 +18,20 @@ get_controls
 
 GAMEDIR=/$directory/ports/exact_chinesechess/
 CONFDIR="$GAMEDIR/conf/"
-RUNDIR="$GAMEDIR/exact_chinesechess"
 
 mkdir -p "$CONFDIR"
 
-cd "$RUNDIR" || exit 1
+cd "$GAMEDIR"
 
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
 export XDG_DATA_HOME="$CONFDIR"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
-export SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS=0
-export LD_LIBRARY_PATH="$RUNDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
 
-BIN="$RUNDIR/exactcc.${DEVICE_ARCH}"
+BIN="$GAMEDIR/exactcc.${DEVICE_ARCH}"
 
-if [ ! -x "$BIN" ]; then
-  echo "Missing binary for DEVICE_ARCH=${DEVICE_ARCH}: $BIN"
-  exit 1
-fi
-
+$GPTOKEYB "exactcc.${DEVICE_ARCH}" -c "$GAMEDIR/exact_chinesechess.gptk" &
 pm_platform_helper "$BIN"
 "$BIN"
 
